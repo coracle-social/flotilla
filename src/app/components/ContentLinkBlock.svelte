d99ada44f5d0a2f9b5f07645682df3872891cd80
Jon Staab <shtaab@gmail.com>
2025-06-16 11:45:05 -0700

    Show link url if no title is available

--- src/app/components/ContentLinkBlock.svelte
+++ src/app/components/ContentLinkBlock.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import {ellipsize, postJson} from "@welshman/lib"
+  import {ellipsize, displayUrl, postJson} from "@welshman/lib"
   import {dufflepud, imgproxy} from "@app/state"
   import {preventDefault, stopPropagation} from "@lib/html"
   import Link from "@lib/components/Link.svelte"
@@ -54,13 +54,11 @@
               src={imgproxy(preview.image)}
               class="bg-alt max-h-72 object-contain object-center" />
           {/if}
-          {#if preview.title}
-            <div class="flex flex-col gap-2 p-4">
-              <strong class="overflow-hidden text-ellipsis whitespace-nowrap"
-                >{preview.title}</strong>
-              <p>{ellipsize(preview.description, 140)}</p>
-            </div>
-          {/if}
+          <div class="flex flex-col gap-2 p-4">
+            <strong class="overflow-hidden text-ellipsis whitespace-nowrap"
+              >{preview.title || displayUrl(url)}</strong>
+            <p>{ellipsize(preview.description, 140)}</p>
+          </div>
         </div>
       {:catch}
         <p class="bg-alt p-12 text-center leading-normal">
