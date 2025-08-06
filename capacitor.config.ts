2a30ca53065c3144f11e75560925aacc382d23ba
Jon Staab <shtaab@gmail.com>
2025-08-06 15:09:22 -0700

    Bump welshman, drop support for safe area insets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,9 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: "https"
   },
+  android: {
+    adjustMarginsForEdgeToEdge: false,
+  },
   plugins: {
     SplashScreen: {
       androidSplashResourceName: "splash"
