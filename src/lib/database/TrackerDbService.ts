8d8a72a89948412417ff950dd8f1774c539c1da4
Matthew Remmel <matt@remmel.dev>
2025-09-09 22:51:17 -0400

    Update TrackersDbService implementation

--- src/lib/database/TrackerDbService.ts
+++ src/lib/database/TrackerDbService.ts
@@ -1,9 +1,17 @@
 import {SQLiteDBConnection} from "@capacitor-community/sqlite"
 import {TrackerMigrationStatements} from "./migrations/tracker.migration.statements"
 import {sqliteService} from "./SQLiteService"
+import type {Tracker} from "@welshman/net"
+import type {Unsubscriber} from "svelte/store"
+
+type Entry = {id: string; relays: string[]}
 
 export interface ITrackerDbService {
   initializeDatabase(): Promise<void>
+  initializeState(): Promise<void>
+  sync(): Unsubscriber
+  getTrackers(): Promise<Entry[]>
+  updateTrackers(entries: Entry[]): Promise<void>
   getDatabaseName(): string
   getDatabaseVersion(): number
 }
@@ -14,6 +22,11 @@ class TrackerDbService implements ITrackerDbService {
   loadToVersion = TrackerMigrationStatements[TrackerMigrationStatements.length - 1].toVersion
   db!: SQLiteDBConnection
   platform = sqliteService.getPlatform()
+  tracker: Tracker
+
+  constructor(tracker: Tracker) {
+    this.tracker = tracker
+  }
 
   async initializeDatabase(): Promise<void> {
     try {
@@ -35,6 +48,61 @@ class TrackerDbService implements ITrackerDbService {
     }
   }
 
+  async initializeState(): Promise<void> {
+    const relaysByid = new Map<string, Set<string>>()
+
+    for (const {id, relays} of await this.getTrackers()) {
+      relaysByid.set(id, new Set(relays))
+    }
+
+    this.tracker.load(relaysByid)
+  }
+
+  sync(): Unsubscriber {
+    const updateOne = (id: string, relay: string) =>
+      this.updateTrackers([{id, relays: Array.from(this.tracker.getRelays(id))}])
+
+    const updateAll = () =>
+      this.updateTrackers(
+        Array.from(this.tracker.relaysById.entries()).map(([id, relays]) => ({
+          id,
+          relays: Array.from(relays),
+        })),
+      )
+
+    this.tracker.on("add", updateOne)
+    this.tracker.on("remove", updateOne)
+    this.tracker.on("load", updateAll)
+    this.tracker.on("clear", updateAll)
+
+    return () => {
+      this.tracker.off("add", updateOne)
+      this.tracker.off("remove", updateOne)
+      this.tracker.off("load", updateAll)
+      this.tracker.off("clear", updateAll)
+    }
+  }
+
+  async getTrackers(): Promise<Entry[]> {
+    const entries = (await this.db.query("SELECT data FROM trackers")).values
+
+    if (entries) {
+      return entries.map(entry => JSON.parse(entry.data) as Entry)
+    } else {
+      return []
+    }
+  }
+
+  async updateTrackers(entries: Entry[]): Promise<void> {
+    const valuesPlaceholder = entries.map(() => "(?, ?)").join(", ")
+    const values = entries.flatMap(entry => [entry.id, JSON.stringify(entry)])
+    await this.db.run(
+      `INSERT INTO trackers (id, data) VALUES ${valuesPlaceholder} ON CONFLICT(id) DO UPDATE SET data = excluded.data WHERE trackers.data IS NOT excluded.data;`,
+      values,
+      true,
+    )
+  }
+
   getDatabaseName(): string {
     return this.databaseName
   }
@@ -43,5 +111,3 @@ class TrackerDbService implements ITrackerDbService {
     return this.loadToVersion
   }
 }
-
-export const trackerDbService = new TrackerDbService()
