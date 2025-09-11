c23b33a8a09b0170b13d20b0e88c4bad0e088619
Matthew Remmel <matt@remmel.dev>
2025-09-11 11:19:48 -0400

    Fix typo in migration statement

--- src/lib/database/migrations/relays.ts
+++ src/lib/database/migrations/relays.ts
@@ -1,6 +1,6 @@
 export const RelaysMigrationStatements = [
   {
     toVersion: 1,
-    statements: [`CREATE TABLE IF NOT EXISTS relays (url TEXT PRIMARY, data TEXT NOT NULL);`],
+    statements: [`CREATE TABLE IF NOT EXISTS relays (url TEXT PRIMARY KEY, data TEXT NOT NULL);`],
   },
 ]
