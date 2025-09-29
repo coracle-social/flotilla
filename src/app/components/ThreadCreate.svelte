e7d6b794050273ab1fd3b2ff2e86b95d3031e06d
Matthew Remmel <matt@remmel.dev>
2025-09-29 14:51:40 -0400

    Add thread edit feature

--- src/app/components/ThreadCreate.svelte
+++ src/app/components/ThreadCreate.svelte
@@ -1,122 +1,27 @@
 <script lang="ts">
-  import {writable} from "svelte/store"
-  import {makeEvent, THREAD} from "@welshman/util"
-  import {publishThunk} from "@welshman/app"
-  import {isMobile, preventDefault} from "@lib/html"
-  import Paperclip from "@assets/icons/paperclip-2.svg?dataurl"
-  import AltArrowLeft from "@assets/icons/alt-arrow-left.svg?dataurl"
-  import Icon from "@lib/components/Icon.svelte"
-  import Field from "@lib/components/Field.svelte"
   import Button from "@lib/components/Button.svelte"
+  import ThreadForm, {type Values} from "@app/components/ThreadForm.svelte"
   import ModalHeader from "@lib/components/ModalHeader.svelte"
-  import ModalFooter from "@lib/components/ModalFooter.svelte"
-  import EditorContent from "@app/editor/EditorContent.svelte"
-  import {pushToast} from "@app/util/toast"
-  import {PROTECTED} from "@app/core/state"
-  import {makeEditor} from "@app/editor"
-  import {canEnforceNip70} from "@app/core/commands"
 
   const {url} = $props()
-
-  const shouldProtect = canEnforceNip70(url)
-
-  const uploading = writable(false)
-
   const back = () => history.back()
-
-  const selectFiles = () => editor.then(ed => ed.commands.selectFiles())
-
-  const submit = async () => {
-    if ($uploading) return
-
-    if (!title) {
-      return pushToast({
-        theme: "error",
-        message: "Please provide a title for your thread.",
-      })
-    }
-
-    const ed = await editor
-    const content = ed.getText({blockSeparator: "\n"}).trim()
-
-    if (!content.trim()) {
-      return pushToast({
-        theme: "error",
-        message: "Please provide a message for your thread.",
-      })
-    }
-
-    const tags = [...ed.storage.nostr.getEditorTags(), ["title", title]]
-
-    if (await shouldProtect) {
-      tags.push(PROTECTED)
-    }
-
-    publishThunk({
-      relays: [url],
-      event: makeEvent(THREAD, {content, tags}),
-    })
-
-    history.back()
-  }
-
-  const editor = makeEditor({url, submit, uploading, placeholder: "What's on your mind?"})
-
-  let title: string = $state("")
 </script>
 
-<form class="column gap-4" onsubmit={preventDefault(submit)}>
-  <ModalHeader>
-    {#snippet title()}
-      <div>Create a Thread</div>
-    {/snippet}
-    {#snippet info()}
-      <div>Share a link, or start a discussion.</div>
-    {/snippet}
-  </ModalHeader>
-  <div class="col-8 relative">
-    <Field>
-      {#snippet label()}
-        <p>Title*</p>
+<ThreadForm url={url}>
+  {#snippet header()}
+    <ModalHeader>
+      {#snippet title()}
+        <div>Create a Thread</div>
       {/snippet}
-      {#snippet input()}
-        <label class="input input-bordered flex w-full items-center gap-2">
-          <!-- svelte-ignore a11y_autofocus -->
-          <input
-            autofocus={!isMobile}
-            bind:value={title}
-            class="grow"
-            type="text"
-            placeholder="What is this thread about?" />
-        </label>
+      {#snippet info()}
+        <div>Share a link, or start a discussion.</div>
       {/snippet}
-    </Field>
-    <Field>
-      {#snippet label()}
-        <p>Message*</p>
-      {/snippet}
-      {#snippet input()}
-        <div class="note-editor flex-grow overflow-hidden">
-          <EditorContent {editor} />
-        </div>
-      {/snippet}
-    </Field>
-    <Button
-      data-tip="Add an image"
-      class="tooltip tooltip-left absolute bottom-1 right-2"
-      onclick={selectFiles}>
-      {#if $uploading}
-        <span class="loading loading-spinner loading-xs"></span>
-      {:else}
-        <Icon icon={Paperclip} size={3} />
-      {/if}
-    </Button>
-  </div>
-  <ModalFooter>
-    <Button class="btn btn-link" onclick={back}>
-      <Icon icon={AltArrowLeft} />
-      Go back
-    </Button>
-    <Button type="submit" class="btn btn-primary">Create Thread</Button>
-  </ModalFooter>
-</form>
+    </ModalHeader>
+  {/snippet}
+  {#snippet footer()}
+    <div class="mt-4 flex flex-row items-center justify-between gap-4">
+      <Button class="btn btn-neutral" onclick={back}>Back</Button>
+      <Button type="submit" class="btn btn-primary">Create Thread</Button>
+    </div>
+  {/snippet}
+</ThreadForm>
---
+++ src/app/components/ThreadForm.svelte
@@ -0,0 +1,130 @@
+<script lang="ts">
+  import {isMobile, preventDefault} from "@lib/html.js"
+  import Paperclip from "@assets/icons/paperclip-2.svg"
+  import EditorContent from "@app/editor/EditorContent.svelte"
+  import Field from "@lib/components/Field.svelte"
+  import Button from "@lib/components/Button.svelte"
+  import Icon from "@lib/components/Icon.svelte"
+  import {makeEditor} from "@app/editor/index.js"
+  import {writable} from "svelte/store"
+  import type {Snippet} from "svelte"
+  import {pushToast} from "@app/util/toast"
+  import {canEnforceNip70} from "@app/core/commands"
+  import {PROTECTED} from "@app/core/state"
+  import {publishThunk} from "@welshman/app"
+  import {makeEvent, THREAD} from "@welshman/util"
+  import {randomId} from "@welshman/lib"
+
+  export type Values = {
+    d?: string,
+    title?: string
+    content?: string
+    tags?: string[][]
+  }
+
+  type Props = {
+    url: string
+    initialValues?: Values
+    header: Snippet
+    footer: Snippet
+  }
+
+  const back = () => history.back()
+
+  const {url, initialValues, header, footer}: Props = $props()
+
+  const values = $state(initialValues || {})
+
+  const shouldProtect = canEnforceNip70(url)
+
+  const submit = async () => {
+    if ($uploading) return
+
+    if (!title) {
+      return pushToast({
+        theme: "error",
+        message: "Please provide a title for your thread.",
+      })
+    }
+
+    const ed = await editor
+    const content = ed.getText({blockSeparator: "\n"}).trim()
+
+    if (!content.trim()) {
+      return pushToast({
+        theme: "error",
+        message: "Please provide a message for your thread.",
+      })
+    }
+
+    const tags = [
+      ["d", values.d || randomId()],
+      ["title", title],
+      ...ed.storage.nostr.getEditorTags(),
+    ]
+
+    if (await shouldProtect) {
+      tags.push(PROTECTED)
+    }
+
+    const event = makeEvent(THREAD, {content, tags})
+
+    pushToast({message: "Your thread has been saved!"})
+    publishThunk({
+      relays: [url],
+      event,
+    })
+
+    back()
+  }
+
+  const uploading = writable(false)
+  const selectFiles = () => editor.then(ed => ed.commands.selectFiles())
+
+  const editor = makeEditor({url, submit, uploading, content: values.content, placeholder: "What's on your mind?"})
+
+  let title: string = $state(values.title || "")
+</script>
+
+<form class="column gap-4" onsubmit={preventDefault(submit)}>
+  {@render header()}
+  <div class="col-8 relative">
+    <Field>
+      {#snippet label()}
+        <p>Title*</p>
+      {/snippet}
+      {#snippet input()}
+        <label class="input input-bordered flex w-full items-center gap-2">
+          <!-- svelte-ignore a11y_autofocus -->
+          <input
+            autofocus={!isMobile}
+            bind:value={title}
+            class="grow"
+            type="text"
+            placeholder="What is this thread about?" />
+        </label>
+      {/snippet}
+    </Field>
+    <Field>
+      {#snippet label()}
+        <p>Message*</p>
+      {/snippet}
+      {#snippet input()}
+        <div class="note-editor flex-grow overflow-hidden">
+          <EditorContent {editor} />
+        </div>
+      {/snippet}
+    </Field>
+    <Button
+      data-tip="Add an image"
+      class="tooltip tooltip-left absolute bottom-1 right-2"
+      onclick={selectFiles}>
+      {#if $uploading}
+        <span class="loading loading-spinner loading-xs"></span>
+      {:else}
+        <Icon icon={Paperclip} size={3} />
+      {/if}
+    </Button>
+  </div>
+  {@render footer()}
+</form>
