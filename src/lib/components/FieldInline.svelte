abd852949b5858ec75bb1ed616d2113a8982aee7
Jon Staab <shtaab@gmail.com>
2026-01-09 09:44:56 -0800

    Tweak breakpoint for field inline

--- src/lib/components/FieldInline.svelte
+++ src/lib/components/FieldInline.svelte
@@ -9,14 +9,14 @@
   const {...props}: Props = $props()
 </script>
 
-<div class="grid grid-cols-1 gap-2 md:grid-cols-3 {props.class}">
+<div class="grid grid-cols-1 gap-2 lg:grid-cols-3 {props.class}">
   <label class="flex items-center gap-2 font-bold">
     {@render props.label?.()}
   </label>
   <div class="col-span-2 flex items-center gap-2">
     {@render props.input?.()}
   </div>
-  <p class="flex-end text-sm opacity-70 md:col-span-3">
+  <p class="flex-end text-sm opacity-70 lg:col-span-3">
     {#if props.info}
       {@render props.info?.()}
     {/if}
