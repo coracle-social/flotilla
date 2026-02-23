edd8824c5ee7666a977f552c39e7d8802f531a67
Jon Staab <shtaab@gmail.com>
2026-02-23 14:40:00 -0800

    Fix safe area inset for modal footer

--- src/lib/components/ModalFooter.svelte
+++ src/lib/components/ModalFooter.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="h-20 flex-shrink-0"></div>
-<div class="flex absolute bottom-0 left-0 right-0 p-6 py-4 rounded-b-box bg-base-200">
+<div class="flex absolute bottom-sai left-0 right-0 p-6 py-4 rounded-b-box bg-base-200">
   <div class="flex flex-grow gap-4 items-center justify-between">
     {@render children?.()}
   </div>
